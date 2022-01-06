4e19ff84a8fa826cb4678e5f2da25bc94634a8f6
SooJ2 <2ecrystal@naver.com>
2022-01-06 15:08:37 +0900

    make tab

--- Assign2/app/src/main/java/com/example/assign2/MainActivity.kt
+++ Assign2/app/src/main/java/com/example/assign2/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.assign2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var calendar:Calendar
+    lateinit var myFeed:MyFeed
+    lateinit var community:Community
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        calendar = Calendar()
+        myFeed = MyFeed()
+        community = Community()
+
+        supportFragmentManager.beginTransaction().add(R.id.frameLayout,calendar).commit()
+
+
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position) {
+                    0 -> {
+                        replaceView(calendar)
+                    }
+                    1 -> {
+                        replaceView(myFeed)
+                    }
+                    2 -> {
+                        replaceView(community)
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+
+    }
+
+    private fun replaceView(tab: Fragment) {
+        var selectedFragment: Fragment? = null
+        selectedFragment = tab
+        selectedFragment?.let {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, it).commit()
+        }
     }
 }
