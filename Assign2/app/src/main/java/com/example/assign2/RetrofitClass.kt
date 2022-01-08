f456940cfe6ba6547bd954ffa80be75fed2260cd
LWJ0126 <wonj29@kaist.ac.kr>
2022-01-08 13:59:05 +0900

    second

--- Assign2/app/src/main/java/com/example/assign2/RetrofitClass.kt
+++ Assign2/app/src/main/java/com/example/assign2/RetrofitClass.kt
@@ -16,46 +16,95 @@ object RetrofitClass {
         .build()
 }
 
-data class User(
-    @SerializedName("id")
-    val userName: String,
-    @SerializedName("")
-    val password: String
-)
+//data class User(
+//    @SerializedName("id")
+//    val id: String,
+//    @SerializedName("")
+//    val password: String
+//)
 
 data class Userinfo(
-    val userId: User,
+    @SerializedName("user_id")
+    val user_id: Int,
+    @SerializedName("private")
     val private: Boolean,
-    val profilePhoto: Image
+    @SerializedName("profile_photo")
+    val profile_photo: Image
 )
+{
+    override fun toString(): String {
+        return "Userinfo{user_id=$user_id, private=$private, profile_photo=$profile_photo}"
+    }
+}
 
 data class Follow(
-    val followingId: User,
-    val followerId: User
+    @SerializedName("following_id")
+    val following_id: Int,
+    @SerializedName("follower_id")
+    val follower_id: Int
 )
+{
+    override fun toString(): String {
+        return "Follow{following_id=$following_id, follower_id=$follower_id}"
+    }
+}
 
 data class Feed(
-    val uploader: User,
-    val feedPhoto: Image,
+    @SerializedName("uploader")
+    val uploader: Int,
+    @SerializedName("feed_photo")
+    val feed_photo: Image,
+    @SerializedName("likes")
     val likes: Int,
-    val uploadTime: DateTimeFormatter,
-    val updateTime: DateTimeFormatter,
-    val eatDate: DatePicker,
-    val dietExplain: String
+    @SerializedName("upload_time")
+    val upload_time: DateTimeFormatter,
+    @SerializedName("update_time")
+    val update_time: DateTimeFormatter,
+    @SerializedName("eat_date")
+    val eat_date: DatePicker,
+    @SerializedName("diet_explain")
+    val diet_explain: String
 )
+{
+    override fun toString(): String {
+        return "Feed{uploader=$uploader, feed_photo=$feed_photo, likes=$likes, upload_time=$upload_time, update_time=$update_time, eat_date=$eat_date, diet_explain=$diet_explain}"
+    }
+}
 
 data class Comment(
+    @SerializedName("feed")
     val feed: Feed,
-    val writer: User,
+    @SerializedName("writer")
+    val writer: Int,
+    @SerializedName("text")
     val text: String
 )
+{
+    override fun toString(): String {
+        return "Comment{feed=$feed, writer=$writer, text=$text}"
+    }
+}
 
 data class Food(
+    @SerializedName("name")
     val name: String,
+    @SerializedName("calorie")
     val calorie: Int
 )
+{
+    override fun toString(): String {
+        return "Food{name=$name, calorie=$calorie}"
+    }
+}
 
 data class EatenFood(
-    val eater: User,
-    val eatenFood: Food
+    @SerializedName("eater")
+    val eater: Int,
+    @SerializedName("eaten_food")
+    val eaten_food: Food
 )
+{
+    override fun toString(): String {
+        return "EatenFood{eater=$eater, eaten_food=$eaten_food}"
+    }
+}
